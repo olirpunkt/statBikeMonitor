8a37fa374c0a05470440396893a0c3e43e0d84f6
haus@andeck.de <haus@andeck.de>
2022-10-01 15:53:40 +0200

    fixed rpm calculation

--- statBikeMonitor.ino
+++ statBikeMonitor.ino
@@ -27,13 +27,16 @@ unsigned long interval=300; //interval for sending data to the websocket server 
 WiFiClient client;
 
 const byte cadencePin = D1; 
-const uint16_t bounceTime = 5; // in ms
+const uint16_t bounceTime = 150; // in ms
 
 const byte speedPin = D2; 
 
 volatile uint32_t cadence_t0 = 0;
 volatile uint32_t cadence_t1 = 0;
 volatile uint32_t cadenceCounter = 0;
+int rpm = 0;
+bool cadenceChanged = false;
+
 
 volatile uint32_t speed_t0 = 0;
 volatile uint32_t speed_t1 = 0;
@@ -44,6 +47,7 @@ ICACHE_RAM_ATTR void cadenceISR() {
     cadence_t0 = cadence_t1;
     cadence_t1 = millis(); 
     cadenceCounter++;  
+    cadenceChanged = true;
   } 
 }
 
@@ -91,63 +95,25 @@ void getCadence() {
   uint32_t t0Local;
 
   cli();
-  cadenceCounterLocal = cadenceCounter;
-  t1Local = t1;
-  t0Local = t0;
+  if (cadenceChanged) { 
+    Serial.print("Count: ");
+    Serial.println(cadenceCounter);
+    rpm = 60000/(cadence_t1 - cadence_t0);
+    Serial.print("time: ");
+    Serial.println(cadence_t1 - cadence_t0);
+    Serial.print("RPM: ");
+    Serial.println(rpm);
+    webSocketClient.sendData((String) rpm);
+    cadenceChanged = false;
+  } /*else {
+    if (millis() - cadence_t1 > 10000) {
+      webSocketClient.sendData((String) 0);
+    }
+    cadenceChanged = false;
+  }*/
   sei();
-  if (cadenceCounterLocal != cadenceCounterOld) {
-    cadenceCounterOld = cadenceCounterLocal;
-    uint32_t cadenceInterval = t1Local - t0Local;
-    uint32_t cadence = 60000/cadenceInterval;
-    Serial.print("Counter: ");Serial.print(cadenceCounterLocal);
-    Serial.print(" Millis: ");Serial.println(t1Local);
-    Serial.print(" took: ");Serial.println(cadenceInterval);
-    Serial.print(" Cadence: ");Serial.println(cadence);
-    webSocketClient.sendData((String) cadence);
-  }
 }
 
-void getValues() {
-  static uint16_t cadenceCounterOld = 0;
-  uint32_t cadenceCounterLocal;
-  uint32_t cadence_t1Local;
-  uint32_t cadence_t0Local;
-
-  static uint16_t speedCounterOld = 0;
-  uint32_t speedCounterLocal;
-  uint32_t speed_t1Local;
-  uint32_t speed_t0Local;
-  
-
-  cli();
-  
-  cadenceCounterLocal = cadenceCounter;
-  cadence_t1Local = cadence_t1;
-  cadence_t0Local = cadence_t0;
-
-  speedCounterLocal = speedCounter;
-  speed_t1Local = speed_t1;
-  speed_t0Local = speed_t0;
-  
-  sei();
-  
-  if (cadenceCounterLocal != cadenceCounterOld) {
-    cadenceCounterOld = cadenceCounterLocal;
-    uint32_t cadenceInterval = t1Local - t0Local;
-    uint32_t cadence = 60000/cadenceInterval;
-    Serial.print("Counter: ");Serial.print(cadenceCounterLocal);
-    Serial.print(" Millis: ");Serial.println(t1Local);
-    Serial.print(" took: ");Serial.println(cadenceInterval);
-    Serial.print(" Cadence: ");Serial.println(cadence);
-    webSocketClient.sendData((String) cadence);
-  }
-
-  if (speedCounterLocal != speedCounterOld) {
-    speedCounterOld = speedCounterLocal;
-    
-  }
-  
-}
 
 
 void loop() {
